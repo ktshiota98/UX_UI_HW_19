c62109263418435b84f2c73f51323898b33b5a73
ktshiota98 <84247741+ktshiota98@users.noreply.github.com>
2021-06-15 16:32:19 -0700

    updated

--- Homework 19/JS/index.js
+++
@@ -1,7 +0,0 @@
-console.log("Your index.js file is loaded correctly!");
-var portfolio= $("li");
-$("portfolio").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".myWork").offset().top
-    }, 5000);
-});
